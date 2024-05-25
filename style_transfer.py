1a61e892abdf9d9c96bf9b9afb27cae4812533ae
eunn25 <rose092504@naver.com>
2024-05-25 19:50:08 +0900

    Performed refactoring in preparation for adding new features

--- style_transfer.py
+++ style_transfer.py
@@ -8,84 +8,98 @@ import matplotlib.pyplot as plt
 import threading
 import os
 
-def load_img(path_to_img):
-    img = plt.imread(path_to_img)
-    img = img.astype(np.float32)[np.newaxis, ...] / 255.
-    return img
+class ImageProcessor:
+    def __init__(self, content_label, style_label, output_label):
+        self.content_path = ""
+        self.style_path = ""
+        self.content_label = content_label
+        self.style_label = style_label
+        self.output_label = output_label
 
-def stylize_image(content_path, style_path):
-    # Load the content and style images.
-    content_image = load_img(content_path)
-    style_image = load_img(style_path)
+    def update_content_path(self):
+        self.content_path = self.select_image(self.content_label)
 
-    # Resize the style image to 256x256.
-    style_image = tf.image.resize(style_image, (256, 256))
+    def update_style_path(self):
+        self.style_path = self.select_image(self.style_label)
 
-    # Load the model.
-    hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+    def select_image(self, image_label):
+        filename, _ = QFileDialog.getOpenFileName() 
+        if filename: 
+            self.set_image_label(filename, image_label)
+        return filename
 
-    # Stylize the image.
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-    stylized_image = outputs[0].numpy()
-
-    return stylized_image
-
-def stylize(content_path, style_path):
-    stylized_image = stylize_image(content_path, style_path)
-    # Convert the stylized image to a PIL image
-    stylized_image = Image.fromarray(np.uint8(stylized_image[0] * 255))
-
-    # Get the original image's size
-    original_image = Image.open(content_path)
-    original_width, original_height = original_image.size
-    content_ratio = original_height / original_width
-    max_display_size = 350
-    if original_width > original_height:
-        new_width = max_display_size
-        new_height = int(new_width * content_ratio)
-    else:
-        new_height = max_display_size
-        new_width = int(new_height / content_ratio)
-
-    # Convert the output image to a display image
-    display_image = stylized_image.resize((new_width, new_height))
-    data = display_image.convert("RGBA").tobytes("raw", "BGRA")
-    qimage = QImage(data, display_image.size[0], display_image.size[1], QImage.Format_ARGB32)
-    pixmap = QPixmap.fromImage(qimage)
-
-    # Update the output label with the stylized image
-    output_label.setPixmap(pixmap)
-
-def stylize_button_click():
-    threading.Thread(target=stylize, args=(content_path, style_path)).start()
-
-def select_image(image_label):
-    filename, _ = QFileDialog.getOpenFileName() 
-    if filename: 
-        image = Image.open(filename)
-        width, height = image.size
-        content_ratio = height / width
+    def set_image_label(self, filename, image_label):
+        image = self.load_img(filename)
+        original_width, original_height = image.size
+        content_ratio = original_height / original_width
         max_display_size = 350
-        if width > height:
+        if original_width > original_height:
             new_width = max_display_size
             new_height = int(new_width * content_ratio)
         else:
             new_height = max_display_size
             new_width = int(new_height / content_ratio)
-        display_image = image.resize((new_width, new_height)) 
+        image = image.resize((new_width, new_height))
+        data = image.convert("RGBA").tobytes("raw", "BGRA")
+        qimage = QImage(data, image.size[0], image.size[1], QImage.Format_ARGB32)
+        pixmap = QPixmap.fromImage(qimage)
+        image_label.setPixmap(pixmap)
+    
+    def load_img(self, path_to_img):
+        img = Image.open(path_to_img)
+        return img
+
+    def stylize_image(self, content_path, style_path):
+        # Load the content and style images.
+        content_image = np.array(self.load_img(content_path))
+        style_image = np.array(self.load_img(style_path))
+
+        # Normalize the images
+        content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
+        style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
+
+        # Resize the style image to 256x256.
+        style_image = tf.image.resize(style_image, (256, 256))
+
+        # Load the model.
+        hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+        # Stylize the image.
+        outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+        stylized_image = outputs[0].numpy()
+
+        return stylized_image
+
+    def stylize(self):
+        stylized_image = self.stylize_image(self.content_path, self.style_path)
+        # Convert the numpy array to PIL Image
+        stylized_image = np.squeeze(stylized_image, axis=0)
+        stylized_image = (stylized_image * 255).astype(np.uint8)
+        stylized_image = Image.fromarray(stylized_image)
+
+        # Get the original image's size
+        original_image = Image.open(self.content_path)  # Changed from content_path to self.content_path
+        original_width, original_height = original_image.size
+        content_ratio = original_height / original_width
+        max_display_size = 350
+        if original_width > original_height:
+            new_width = max_display_size
+            new_height = int(new_width * content_ratio)
+        else:
+            new_height = max_display_size
+            new_width = int(new_height / content_ratio)
+
+        # Convert the output image to a display image
+        display_image = stylized_image.resize((new_width, new_height))
         data = display_image.convert("RGBA").tobytes("raw", "BGRA")
         qimage = QImage(data, display_image.size[0], display_image.size[1], QImage.Format_ARGB32)
         pixmap = QPixmap.fromImage(qimage)
-        image_label.setPixmap(pixmap)
-    return filename
 
-def update_content_path():
-    global content_path
-    content_path = select_image(content_label)
+        # Update the output label with the stylized image
+        self.output_label.setPixmap(pixmap)
 
-def update_style_path():
-    global style_path
-    style_path = select_image(style_label)
+    def stylize_button_click(self):
+        threading.Thread(target=self.stylize).start()
 
 app = QApplication([])
 
@@ -94,26 +108,26 @@ layout = QGridLayout()
 
 button_font = QFont("Helvetica", 10)
 
-content_path = ""
-style_path = ""
-
 content_label = QLabel()
 content_button = QPushButton('Select Content Image')
 content_button.setFont(button_font)
 content_button.setStyleSheet("background-color: skyblue")
-content_button.clicked.connect(update_content_path)
 
 style_label = QLabel()
 style_button = QPushButton('Select Style Image')
 style_button.setFont(button_font)
 style_button.setStyleSheet("background-color: skyblue")
-style_button.clicked.connect(update_style_path)
 
 output_label = QLabel()
 stylize_button = QPushButton('Stylize')
 stylize_button.setFont(button_font)
 stylize_button.setStyleSheet("background-color: skyblue")
-stylize_button.clicked.connect(lambda: threading.Thread(target=stylize, args=(content_path, style_path)).start())
+
+image_processor = ImageProcessor(content_label, style_label, output_label)
+
+content_button.clicked.connect(image_processor.update_content_path)
+style_button.clicked.connect(image_processor.update_style_path)
+stylize_button.clicked.connect(image_processor.stylize_button_click)
 
 layout.addWidget(content_label, 0, 0) # Content image label
 layout.addWidget(output_label, 0, 1) # Output image label
