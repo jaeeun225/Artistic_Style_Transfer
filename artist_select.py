a28da56d4597f426ef4862a18c8eb72ab6dadbd1
eunn25 <rose092504@naver.com>
2024-05-25 23:56:00 +0900

    Add my artist page

--- artist_select.py
+++ artist_select.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QMainWindow, QGridLayout, QSpacerItem, QSizePolicy
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
-
+from my_artist import MyArtistPage
 class ArtistSelect(QMainWindow):
     def __init__(self, main_page):
         super().__init__()
@@ -9,7 +9,7 @@ class ArtistSelect(QMainWindow):
         self.main_page = main_page
 
         self.setWindowTitle("Artist Select")
-        self.setFixedSize(500, 650)
+        self.setFixedSize(500, 700)
 
         widget = QWidget()
 
@@ -17,6 +17,7 @@ class ArtistSelect(QMainWindow):
         gallery_button.setFixedSize(320, 200)  # Set the size of the button
         my_artist_button = QPushButton('나의 화가 선택하기')
         my_artist_button.setFixedSize(320, 200)  # Set the size of the button
+        my_artist_button.clicked.connect(self.open_my_artist_page)
 
         back_button = QPushButton('뒤로 가기')
         back_button.setFixedSize(320, 80)  # Set the size of the button
@@ -31,6 +32,11 @@ class ArtistSelect(QMainWindow):
         widget.setLayout(button_layout)
         widget.setContentsMargins(30, 30, 30, 30)  # Set the border
         self.setCentralWidget(widget)
+
+    def open_my_artist_page(self):
+        self.hide()  # Hide the current page
+        self.my_artist_page = MyArtistPage(self)  # Create the MyArtistPage
+        self.my_artist_page.show()  # Show the MyArtistPage
 
     def go_back(self):
         self.close()  # Close the current page
---
+++ my_artist.py
@@ -0,0 +1,174 @@
+from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QMainWindow, QGridLayout, QSpacerItem, QSizePolicy, QLabel, QFileDialog
+from PyQt5.QtGui import QFont, QPixmap, QImage
+from PyQt5.QtCore import Qt
+from PIL import Image
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import threading
+import os
+from my_artist_result import MyArtistResult
+
+class ImageProcessor:
+    def __init__(self, content_label, style_label, output_label):
+        self.content_path = ""
+        self.style_path = ""
+        self.content_label = content_label
+        self.style_label = style_label
+        self.output_label = output_label
+    
+    def update_content_path(self):
+        self.content_path = self.select_image(self.content_label)
+
+    def update_style_path(self):
+        self.style_path = self.select_image(self.style_label)
+
+    def select_image(self, image_label):
+        filename, _ = QFileDialog.getOpenFileName() 
+        if filename: 
+            self.set_image_label(filename, image_label)
+        return filename
+
+    def set_image_label(self, filename, image_label):
+        image = self.load_img(filename)
+        original_width, original_height = image.size
+        content_ratio = original_height / original_width
+        max_display_size = 200
+        if original_width > original_height:
+            new_width = max_display_size
+            new_height = int(new_width * content_ratio)
+        else:
+            new_height = max_display_size
+            new_width = int(new_height / content_ratio)
+        image = image.resize((new_width, new_height))
+        data = image.convert("RGBA").tobytes("raw", "BGRA")
+        qimage = QImage(data, image.size[0], image.size[1], QImage.Format_ARGB32)
+        pixmap = QPixmap.fromImage(qimage)
+        image_label.setPixmap(pixmap)
+
+    def load_img(self, path_to_img):
+        img = Image.open(path_to_img)
+        return img
+
+    def stylize_image(self, content_path, style_path):
+        # Load the content and style images.
+        content_image = np.array(self.load_img(content_path))
+        style_image = np.array(self.load_img(style_path))
+
+        # Normalize the images
+        content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
+        style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
+
+        # Resize the style image to 256x256.
+        style_image = tf.image.resize(style_image, (256, 256))
+
+        # Load the model.
+        hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+        # Stylize the image.
+        outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+        stylized_image = outputs[0].numpy()
+
+        return stylized_image
+
+    def stylize(self):
+        stylized_image = self.stylize_image(self.content_path, self.style_path)
+        # Convert the numpy array to PIL Image
+        stylized_image = np.squeeze(stylized_image, axis=0)
+        stylized_image = (stylized_image * 255).astype(np.uint8)
+        stylized_image = Image.fromarray(stylized_image)
+
+        # Save the stylized image and return its path
+        output_image_path = "output.jpg"
+        stylized_image.save(output_image_path)
+
+        # Get the original image's size
+        original_image = Image.open(self.content_path)  # Changed from content_path to self.content_path
+        original_width, original_height = original_image.size
+        content_ratio = original_height / original_width
+        max_display_size = 200
+        if original_width > original_height:
+            new_width = max_display_size
+            new_height = int(new_width * content_ratio)
+        else:
+            new_height = max_display_size
+            new_width = int(new_height / content_ratio)
+
+        # Convert the output image to a display image
+        display_image = stylized_image.resize((new_width, new_height))
+        data = display_image.convert("RGBA").tobytes("raw", "BGRA")
+        qimage = QImage(data, display_image.size[0], display_image.size[1], QImage.Format_ARGB32)
+        pixmap = QPixmap.fromImage(qimage)
+
+        # Update the output label with the stylized image
+        self.output_label.setPixmap(pixmap)
+
+        return output_image_path
+
+    def stylize_button_click(self):
+        output_image_path = self.stylize()
+        # 결과 페이지 생성 및 표시
+        self.result_page = MyArtistResult(output_image_path)
+        self.result_page.show()
+
+class MyArtistPage(QMainWindow):
+    def __init__(self, previous_page):
+        super().__init__()
+
+        self.previous_page = previous_page
+
+        self.setWindowTitle("My Artist Page")
+        self.setFixedSize(500, 700)
+
+        widget = QWidget()
+        layout = QVBoxLayout()
+
+        self.content_label = QLabel()
+        self.style_label = QLabel()
+        self.output_label = QLabel()
+
+        self.image_processor = ImageProcessor(self.content_label, self.style_label, self.output_label)
+
+        content_button = QPushButton('바꿀 이미지 선택하기')
+        content_button.setFixedSize(320, 40)
+        content_button.clicked.connect(self.image_processor.update_content_path)
+
+        style_button = QPushButton('원하는 스타일 선택하기')
+        style_button.setFixedSize(320, 40)
+        style_button.clicked.connect(self.image_processor.update_style_path)
+
+        stylize_button = QPushButton('작품 만들기')
+        stylize_button.setFixedSize(320, 40)
+        stylize_button.clicked.connect(self.image_processor.stylize_button_click)
+
+        back_button = QPushButton('뒤로 가기')
+        back_button.setFixedSize(320, 40)  # Set the size of the button
+        back_button.clicked.connect(self.go_back)  # Connect the button click event to the handler
+
+        layout.addLayout(self.centered_layout(self.content_label))
+        layout.addItem(QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Fixed))  # Add spacer
+        layout.addLayout(self.centered_layout(content_button))
+        layout.addItem(QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Fixed))  # Add spacer
+        layout.addLayout(self.centered_layout(self.style_label))
+        layout.addItem(QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Fixed))  # Add spacer
+        layout.addLayout(self.centered_layout(style_button))
+        layout.addItem(QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Fixed))  # Add spacer
+        layout.addLayout(self.centered_layout(stylize_button))
+        layout.addItem(QSpacerItem(5, 5, QSizePolicy.Minimum, QSizePolicy.Fixed))  # Add spacer
+        layout.addLayout(self.centered_layout(back_button))
+
+        widget.setLayout(layout)
+        widget.setContentsMargins(30, 30, 30, 30)  # Set the border
+        self.setCentralWidget(widget)
+
+    def go_back(self):
+        self.close()  # Close the current page
+        self.previous_page.show()  # Show the previous page
+
+    def centered_layout(self, widget):
+        hbox = QHBoxLayout()
+        hbox.addStretch()
+        hbox.addWidget(widget)
+        hbox.addStretch()
+        return hbox
